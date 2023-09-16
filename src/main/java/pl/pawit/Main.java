c82bc2f4c1cb05ed66b4e3d402c467b35c1c62d8
PawIwIT <iwan327@wp.pl>
2023-09-16 15:28:10 +0200

    wiadomosc dla kolaboranta

--- src/main/java/pl/pawit/Main.java
+++ src/main/java/pl/pawit/Main.java
@@ -3,5 +3,7 @@ package pl.pawit;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        System.out.println("Witaj kolaborancie");
     }
 }
